d964e873d1355206f3b254b62ee5e015c5572d5c
Erik Mikac <mikacew@gmail.com>
2021-05-16 16:42:30 -0400

    Initial Commit

---
+++ Shared/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  Shared
+//
+//  Created by Erik Mikac on 5/16/21.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
