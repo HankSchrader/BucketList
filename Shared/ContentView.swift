7f5978c57aac3a9b999f70ab3fa4615119fe14a0
Erik Mikac <mikacew@gmail.com>
2021-05-16 17:25:19 -0400

    Reverted master.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,26 +6,11 @@
 //
 
 import SwiftUI
-struct User: Identifiable, Comparable {
-    static func < (lhs: User, rhs: User) -> Bool {
-        lhs.lastName < rhs.lastName
-    }
-    
-    let id = UUID()
-    let firstName: String
-    let lastName: String
-}
+
 struct ContentView: View {
-    let users = [
-        User(firstName: "Arnold", lastName: "Rimmer"),
-        User(firstName: "Kristine", lastName: "Kochanski"),
-        User(firstName: "David", lastName: "Lister"),
-    ].sorted()
 
     var body: some View {
-        List(users) { user in
-            Text("\(user.lastName), \(user.firstName)")
-        }
+        Text("Hello World.")
     }
 }
 
